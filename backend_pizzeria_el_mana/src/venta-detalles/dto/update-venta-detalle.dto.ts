415d50c258a20251f97ee7b39a8f46868aed4c17
Javier <raularamayoramirez77@gmail.com>
2025-11-07 14:30:07 -0400

    arreglando carpetas

--- backend_pizzeria_el_mana/src/venta-detalles/dto/update-venta-detalle.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVentaDetalleDto } from './create-venta-detalle.dto';
-
-export class UpdateVentaDetalleDto extends PartialType(CreateVentaDetalleDto) {}
